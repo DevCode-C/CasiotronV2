aa86481dfc8126c5cfcf5afad3b427dfc4031bf0
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-07-19 03:32:26 -0500

    Scheduler determinista almost added

--- app/main.c
+++ app/main.c
@@ -50,22 +50,49 @@ uint32_t WWDGTick               = 0;
 uint16_t hearBeatTickTime       = 300U;
 WWDG_HandleTypeDef WWDG_HandleInit  = {0};
 TIM_HandleTypeDef TimHandle;
+static uint32_t counter = 0;
 
+uint32_t task1 = 0;
+uint32_t task2 = 0;
+uint32_t task3 = 0;
+uint32_t task4 = 0;
 int main( void )
 {
     HAL_Init( );
-    // serial_init();
+    task1 = counter;
+    serial_init();
     heart_init();
-    // clock_init();
-    // dog_init();
+    task2 = counter;
+    clock_init();
     timer_Init();
+    dog_init();
     for (; ;)
     {
-        // serial_Task();
-        // clock_task();
+
+        if (counter - task1 >= 10)
+        {
+            task1 = counter;
+            serial_Task();
+        }
+
+        if (counter - task2 >= 50)
+        {
+            task2 = counter;
+            clock_task();
+        }
         
-        // heart_beat();
-        // peth_the_dog();
+        
+        if (counter - task3 >= 5)
+        {
+            task3 = counter;
+            heart_beat();
+        }
+        
+        if (counter - task4 >= 4)
+        {
+            task4 = counter;
+            peth_the_dog();
+        }
     } 
     return 0u;
 }
@@ -81,6 +108,8 @@ void heart_init(void)
     GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(GPIO_LED_PORT_BOARD,&GPIO_InitStructure);
     hearBeatTick = HAL_GetTick();
+    task3 = counter;
+
 }
 void heart_beat(void)
 {
@@ -99,13 +128,13 @@ void dog_init(void)
     WWDG_HandleInit.Init.Counter = 127; 
     WWDG_HandleInit.Init.EWIMode = WWDG_EWI_DISABLE;
     HAL_WWDG_Init(&WWDG_HandleInit);
-    
+    task4 = counter;
     WWDGTick = HAL_GetTick();
 }
 
 void peth_the_dog(void)
 {
-    if ((HAL_GetTick() - WWDGTick) >= 40)
+    if ((HAL_GetTick() - WWDGTick) >= 35)
     {
         WWDGTick = HAL_GetTick();
         HAL_WWDG_Refresh(&WWDG_HandleInit);
@@ -116,7 +145,7 @@ void peth_the_dog(void)
 void timer_Init(void)
 {
     uint32_t prescalerValue = 0;
-    prescalerValue = (uint32_t)(SystemCoreClock/10000) - 1;
+    prescalerValue = (uint32_t)(SystemCoreClock/100000) - 1;
 
     TimHandle.Instance = TIM3;
     TimHandle.Init.Period = 1000;
@@ -128,9 +157,16 @@ void timer_Init(void)
     HAL_TIM_Base_Init(&TimHandle);
 
     HAL_TIM_Base_Start_IT(&TimHandle);
+    
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    HAL_GPIO_TogglePin(GPIO_LED_PORT_BOARD,GPIO_LED_PIN_BOARD);
+    // if (counter == 100)
+    // {
+    //     counter=0;
+    //     HAL_GPIO_TogglePin(GPIO_LED_PORT_BOARD,GPIO_LED_PIN_BOARD);    
+    // }
+    counter += 1;
+    
 }
